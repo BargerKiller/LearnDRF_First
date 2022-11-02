0e83a7e88c9304cc63ad6b41c2ae41b208c1d6d8
Bol4onok <Bol4onok@mail.com>
2022-11-02 18:11:58 +0300

    some features change

--- py_client/delete.py
+++ py_client/delete.py
@@ -4,5 +4,5 @@ product_id = 5
 endpoint = f"http://localhost:8000/api/products/{product_id}/delete/"
 
 get_response = requests.delete(endpoint)
-# print(get_response.text)
+
 print(get_response.status_code)
